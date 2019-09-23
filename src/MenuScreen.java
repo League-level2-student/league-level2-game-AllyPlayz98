10882f4c95fb5a0871d547680d256ff04fe3bc90
League Student <league@iMac-34.attlocal.net>
2019-09-23 16:54:47 -0700

    ZodiacScramble ip (Continuing work on title screen)

--- src/MenuScreen.java
+++ src/MenuScreen.java
@@ -6,6 +6,8 @@ import javax.swing.JLabel;
 
 public class MenuScreen extends Screen {
     JLabel label;
+    JLabel inslabel;
+    JLabel startlabel;
     Color textColor;
 
     public MenuScreen(JFrame frame) {
@@ -13,15 +15,27 @@ public class MenuScreen extends Screen {
         backgroundColor = new Color(170, 136, 255);
         textColor = new Color (51, 136, 221);
         label = new JLabel();
+        inslabel = new JLabel();
+        startlabel = new JLabel();
     }
 
     public void drawMenu() {
         frame.add(panel);
         panel.add(label);
         panel.setBackground(backgroundColor);
+        
         label.setFont(new Font( "Serif", Font.PLAIN, 40));
         label.setForeground(textColor);
         label.setText("ZODIAC SCRAMBLE");
+        
+        inslabel.setFont(new Font( "Serif", Font.PLAIN, 35));
+        inslabel.setForeground(textColor);
+        inslabel.setText("Press SPACE for intstructions");
+        
+        startlabel.setFont(new Font( "Serif", Font.PLAIN, 35));
+        startlabel.setForeground(textColor);
+        startlabel.setText("Press ENTER to start");
+        
         panel.repaint();
         frame.pack();
     }
