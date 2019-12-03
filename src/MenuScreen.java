bef86140693dd2bc2f8e5860f6460bc589281c25
League Student <league@iMac-34.attlocal.net>
2019-12-02 16:58:20 -0800

    ZodiacScramble ip

--- src/MenuScreen.java
+++ src/MenuScreen.java
@@ -6,56 +6,56 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class MenuScreen extends Screen {
-    JLabel label;
-    JLabel inslabel;
-    JLabel startlabel;
-    Color textColor;
+	JLabel label;
+	JLabel inslabel;
+	JLabel startlabel;
+	Color textColor;
 
-    public MenuScreen(JFrame frame) {
-        super(frame);
-        backgroundColor = new Color(170, 136, 255);
-        textColor = new Color (51, 136, 221);
-        label = new JLabel();
-        inslabel = new JLabel();
-        startlabel = new JLabel();
-    }
+	public MenuScreen(JFrame frame) {
+		super(frame);
+		backgroundColor = new Color(170, 136, 255);
+		textColor = new Color(51, 136, 221);
+		label = new JLabel();
+		inslabel = new JLabel();
+		startlabel = new JLabel();
+	}
 
-    public void draw() {
-    	//c.fill = GridBagConstraints.HORIZONTAL;
-    	c.gridx = 0;
-    	c.gridy = 0;
-    	panel.add(label, c);
-    	
-    	//c.fill = GridBagConstraints.HORIZONTAL;
-    	c.gridx = 0;
-    	c.gridy = 1;
-    	panel.add(inslabel, c);
-    	
-    	//c.fill = GridBagConstraints.HORIZONTAL;
-    	c.gridx = 0;
-    	c.gridy = 2;
-    	panel.add(startlabel, c);
-    	
-        frame.add(panel);
-        //panel.add(inslabel);
-        //panel.add(startlabel);
-        //panel.add(label);
-        panel.setBackground(backgroundColor);
-        
-        label.setFont(new Font( "Serif", Font.PLAIN, 40));
-        label.setForeground(textColor);
-        label.setText("ZODIAC SCRAMBLE");
-        
-        inslabel.setFont(new Font( "Serif", Font.PLAIN, 35));
-        inslabel.setForeground(textColor);
-        inslabel.setText("Press ENTER for instructions");
-        
-        startlabel.setFont(new Font( "Serif", Font.PLAIN, 35));
-        startlabel.setForeground(textColor);
-        startlabel.setText("Press SPACE to start");
-        
-        panel.repaint();
-        frame.pack();
-    }
+	public void draw() {
+		// c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 0;
+		panel.add(label, c);
+
+		// c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 1;
+		panel.add(inslabel, c);
+
+		// c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 2;
+		panel.add(startlabel, c);
+
+		frame.add(panel);
+		// panel.add(inslabel);
+		// panel.add(startlabel);
+		// panel.add(label);
+		panel.setBackground(backgroundColor);
+
+		label.setFont(new Font("Serif", Font.PLAIN, 40));
+		label.setForeground(textColor);
+		label.setText("ZODIAC SCRAMBLE");
+
+		inslabel.setFont(new Font("Serif", Font.PLAIN, 35));
+		inslabel.setForeground(textColor);
+		inslabel.setText("Press ENTER for instructions");
+
+		startlabel.setFont(new Font("Serif", Font.PLAIN, 35));
+		startlabel.setForeground(textColor);
+		startlabel.setText("Press SPACE to start");
+
+		panel.repaint();
+		frame.pack();
+	}
 
 }
